48c615d1114a982dd9dc2a839eeeebe3c5333c23
rola <luming_bfm@163.com>
2016-08-18 19:03:42 +0800

    后台部分页面增加

---
+++ src/main/webapp/easyui-1.3.5/js/ApplicationData.js
@@ -0,0 +1,27 @@
+/**
+ * Created by rola on 2016/8/18.
+ */
+// 后台树菜单数据
+var tree_data = [
+    {
+        text:"系统维护",
+        state:"closed",
+        children:[{
+            text:"系统监控",
+            attributes:{
+                url:""
+            }
+        },{
+            text:"数据库维护",
+            attributes:{
+                url:""
+            }
+        },{
+            text:"数据库连接池监控",
+            attributes:{
+                url:""
+            }
+        }]
+    }
+];
+
